89964b2df2652dbce59c33006ebd1373834ce8fa
Ethan Lin <ethan.lin@uwex.edu>
2019-04-26 14:45:29 -0500

    updated file structure

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "build/css"
 sass_dir = "css"
 images_dir = "images"
 javascripts_dir = "scripts"
